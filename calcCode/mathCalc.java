e4b7e9ecf08f1e0b593590cb3ed4be52f3dfa8bc
technicalace <80608826+technicalace@users.noreply.github.com>
2022-03-03 16:55:38 -0600

    First Version

--- calcCode/mathCalc.java
+++ calcCode/mathCalc.java
@@ -1,25 +1,54 @@
 package calcCode;
-import java.util.Scanner; 
+import java.util.Scanner;
+
 
 public class mathCalc {
-    public String name; 
-    public int x; 
-    public int y;
+    public static String name; 
+    public static int x; 
+    public static int y;
+    public static int result; 
     
     public static void main(String[] args) {
         // introduction 
         Scanner userInput = new Scanner(System.in); 
         System.out.println("Hello, welcome to the program. What is your name: ");
 
-        String name = userInput.nextLine(); 
+        name = userInput.nextLine(); 
         System.out.println("Hello " + name + " welcome to the program.");
 
-        // equations 
+        // entering number values 
+        System.out.println("Choose your first number: ");
+        x = userInput.nextInt(); 
+        System.out.println("Choose your second number: ");
+        y = userInput.nextInt();
+
         System.out.println( "What form of operation would you like to use?: ");
         char operator = userInput.next().charAt(0); 
 
-        if (operator == '-') {
-            System.out.println(operator);
+        // equations
+        switch (operator) {
+            case '+': 
+                result = x + y;
+                System.out.println(x + " + " + y + " = " + result);
+                break; 
+            case '-': 
+                result = x - y; 
+                System.out.println(x + " - " + y + " = " + result);
+                break;
+            case '*': 
+                result = x * y; 
+                System.out.println(x + " * " + y + " = " + result);
+                break; 
+            case '/': 
+                result = x / y; 
+                System.out.println(x + " / " + y + " = " + result);
+                break; 
+            case '%': 
+                result = x % y; 
+                System.out.println(x + " % " + y + " = " + result);
+                break; 
+            default: 
+                System.out.println("That is not a valid character, try again.");
         }
 
         userInput.close();
