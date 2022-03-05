3df6582304971eacb8747248b47b209ff7252668
technicalace <80608826+technicalace@users.noreply.github.com>
2022-03-05 02:16:00 -0600

    soacahtoa setup 2

--- calcCode/soaCahToa.java
+++ calcCode/soaCahToa.java
@@ -8,9 +8,19 @@ public class soaCahToa {
     void hasAngle() {
         Scanner userInput = new Scanner(System.in); 
 
-        System.out.println("Does your triangle have an angle? ");
+        System.out.println("Does your triangle have an angle?\nType (y/n): ");
         char userResp = userInput.next().charAt(0); 
-        
+
+        if (userResp == 'y') {
+            inputValuesWithAng();
+        }
+    }
+
+    void inputValuesWithAng() {
+
+    }
+
+    void inputValuesWithoutAng() {
 
     }
 }
