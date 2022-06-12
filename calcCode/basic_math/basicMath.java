6ef39a4734b6a5782fcadfcbc08e3c20e3cd9c26
technicalace <80608826+technicalace@users.noreply.github.com>
2022-06-11 20:22:00 -0500

    Add: Implementing math tables to basic math (setup)

--- calcCode/basic_math/basicMath.java
+++ calcCode/basic_math/basicMath.java
@@ -7,14 +7,18 @@ public class basicMath { // primary variables
     public void basicM() { // does not need switch case 
         Scanner scan = new Scanner(System.in); 
         simpEq se = new simpEq();
+        calcTables ct = new calcTables(); 
 
-        System.out.println("What type of equations would you like to solve for?:\n1.) Basic Math");
+        System.out.println("What type of equations would you like to solve for?:\n1.) Basic Math\n2.) Math Tables");
 
         input = scan.nextInt();
         
         if (input == 1) {
             System.out.println("======================================");
             se.setNums();
+        } else if (input == 2) {
+            System.out.println("======================================");
+            ct.portTable();
         } else {
             System.out.println("======================================");
             System.out.println("That is not a valid input, please try again.");
---
+++ calcCode/basic_math/calcTables.java
@@ -0,0 +1,25 @@
+package calcCode.basic_math;
+import java.util.Scanner;
+
+public class calcTables {
+    public void portTable() {
+        System.out.println("You have chosen math tables. What would you like to choose?");
+        System.out.println("1.) Addition Tables\n2.) Subtraction Tables\n3.) Multiplication Tables\n4.) Division Tables");
+    }
+
+    void addTables() {
+
+    }
+
+    void subTables() {
+
+    }
+
+    void mulTables() {
+
+    }
+
+    void divTables() {
+
+    }
+}
