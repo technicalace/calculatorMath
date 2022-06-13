651f1a5aa5f2715c00fb820f2ad30a155531b328
technicalace <80608826+technicalace@users.noreply.github.com>
2022-06-13 16:21:55 -0500

    Add: calcTables.java finished (open to changes)

--- calcCode/basic_math/calcTables.java
+++ calcCode/basic_math/calcTables.java
@@ -1,25 +1,88 @@
 package calcCode.basic_math;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class calcTables {
     public void portTable() {
+        Scanner scan = new Scanner(System.in);
+        int uResp; 
+        
         System.out.println("You have chosen math tables. What would you like to choose?");
-        System.out.println("1.) Addition Tables\n2.) Subtraction Tables\n3.) Multiplication Tables\n4.) Division Tables");
+        System.out.println("1.) Addition Tables\n2.) Subtraction Tables\n3.) Multiplication Tables");
+
+        uResp = scan.nextInt();
+        switch (uResp) {
+            case 1: 
+                System.out.println("======================================");
+                addTables();
+                break;
+            case 2: 
+                System.out.println("======================================");
+                subTables();
+                break;
+            case 3: 
+                System.out.println("======================================");
+                mulTables();
+                break; 
+            default: 
+                System.out.println("======================================");
+                System.out.println("That input is out of bounds, please try again.");
+                portTable();
+        }
+        scan.close();
     }
 
     void addTables() {
-
+        Scanner scan = new Scanner(System.in);
+        int i, uResp;
+        
+        try {
+            System.out.println("Input a number: ");
+            uResp = scan.nextInt();
+            for (i = 1; i < 21; i++) {
+                System.out.printf("%d + %d = %d\n", uResp, i, uResp + i);
+            }
+        } catch (InputMismatchException ie) {
+            System.out.println("======================================");
+            System.out.println("That is an invalid input, please try again.");
+            addTables();
+        }
+        scan.close();
     }
 
     void subTables() {
+        Scanner scan = new Scanner(System.in);
+        int i, uResp;
 
+        try {
+            System.out.println("Input a number: ");
+            uResp = scan.nextInt();
+            for (i = 1; i < 21; i++) {
+                System.out.printf("%d - %d = %d\n", uResp, i, uResp - i);
+            }
+        } catch (InputMismatchException ie) {
+            System.out.println("======================================");
+            System.out.println("That is not a valid input, please try again.");
+            subTables();
+        }
+        scan.close();
     }
 
     void mulTables() {
+        Scanner scan = new Scanner(System.in);
+        int i, uResp;
 
-    }
-
-    void divTables() {
-
-    }
+        try {
+            System.out.println("Input a number: ");
+            uResp = scan.nextInt(); 
+            for (i = 1; i < 21; i++) {
+                System.out.printf("%d * %d = %d\n", uResp, i, uResp * i);
+            }
+        } catch (InputMismatchException ie) {
+            System.out.println("======================================");
+            System.out.println("That is not a valid input, please try again.");
+            mulTables();
+        }
+        scan.close();
+    }   
 }
